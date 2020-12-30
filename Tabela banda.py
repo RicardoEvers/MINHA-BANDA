a7bf0d94e7f482a5d3fbdaea843b27229232da89
Ricardo Bastos Evers de Sousa <ricardo.bastos.evers@gmail.com>
2020-12-30 11:59:31 -0300

    Tabela experimental com tema de minha banda para teste

---
+++ Tabela banda.py
@@ -0,0 +1,22 @@
+lista_h = ['Victor', 'Tiago', 'Murilo', 'Ricardo', 'Ivan',
+         'Lucas', 'Ido', 'Rodrigo', 'Gabriel']
+instrumento_h = ['Voz', 'Guitarra', 'Baixo', 'Bateria']
+atual_h = ['Sim', 'Não']
+
+
+def humanics():
+    print('\t\t--BANDA HUMANICS--')
+    print('\tNome\t\tInstrumento\t\tAtual')
+    print('-------------------------------------')
+    print(0, lista_h[0], '\t\t', instrumento_h[0], '\t\t\t', atual_h[0])
+    print(1, lista_h[1], '\t\t', instrumento_h[1], '\t\t', atual_h[0])
+    print(2, lista_h[2], '\t\t', instrumento_h[1], '\t\t', atual_h[0])
+    print(3, lista_h[3], '\t\t', instrumento_h[2], '\t\t\t', atual_h[0])
+    print(4, lista_h[4], '\t\t\t', instrumento_h[3], '\t\t', atual_h[0])
+    print(5, lista_h[5], '\t\t', instrumento_h[3], '\t\t', atual_h[1])
+    print(6, lista_h[6], '\t\t\t', instrumento_h[1], '\t\t', atual_h[1])
+    print(7, lista_h[7], '\t\t', instrumento_h[2], '\t\t\t', atual_h[1])
+    print(8, lista_h[8], '\t\t', instrumento_h[3], '\t\t', atual_h[1])
+
+
+humanics()
